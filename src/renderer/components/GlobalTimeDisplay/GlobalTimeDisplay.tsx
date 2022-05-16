731b7d5722ef4cd68a7e4d778f915835d0ca3dfc
Sam Richardson <sam@richardson.co.nz>
2022-05-16 13:07:25 +1000

    Fix warnings

--- src/renderer/components/GlobalTimeDisplay/GlobalTimeDisplay.tsx
+++ src/renderer/components/GlobalTimeDisplay/GlobalTimeDisplay.tsx
@@ -1,15 +1,18 @@
+import { Text } from '@chakra-ui/react';
 import useStore from '../../services/stores/videos';
 
 import { secondsToHms } from '../../services/time';
-
-import { Text } from '@chakra-ui/react';
 
 export default function GlobalTimeDisplay() {
   const currentTime = useStore((state) => state.currentTime);
   const fullDuration = useStore((state) => state.fullDuration);
 
+  if (fullDuration === null) {
+    return null;
+  }
+
   return (
-    <Text whiteSpace={'nowrap'} fontSize={'sm'} align={'center'}>
+    <Text whiteSpace="nowrap" fontSize="sm" align="center">
       {secondsToHms(Math.round(currentTime))} /{' '}
       {secondsToHms(Math.round(fullDuration))}
     </Text>
--- src/renderer/components/NavLink/NavLink.tsx
+++ src/renderer/components/NavLink/NavLink.tsx
@@ -1,12 +1,15 @@
-import { useMatch, useResolvedPath } from 'react-router-dom';
+import {
+  useMatch,
+  useResolvedPath,
+  Link as RouterLink,
+} from 'react-router-dom';
 import type { LinkProps } from 'react-router-dom';
 
 import { Link } from '@chakra-ui/react';
-import { Link as RouterLink } from 'react-router-dom';
 
 export default function NavLink({ children, to, ...props }: LinkProps) {
-  let resolved = useResolvedPath(to);
-  let match = useMatch({ path: resolved.pathname, end: true });
+  const resolved = useResolvedPath(to);
+  const match = useMatch({ path: resolved.pathname, end: true });
 
   return (
     <div>
@@ -14,11 +17,12 @@ export default function NavLink({ children, to, ...props }: LinkProps) {
         as={RouterLink}
         backgroundColor={match ? 'whiteAlpha.400' : 'none'}
         to={to}
-        px={'4'}
-        py={'2'}
-        borderRadius={'2xl'}
-        ml={'4'}
+        px="4"
+        py="2"
+        borderRadius="2xl"
+        ml="4"
         _focus={{ boxShadow: 'none' }}
+        // eslint-disable-next-line react/jsx-props-no-spreading
         {...props}
       >
         {children}
