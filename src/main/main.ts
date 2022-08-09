225d2855a0caefcdf3437a2f584b9199130d0f35
Sam Richardson <sam@richardson.co.nz>
2022-08-09 22:24:41 +1000

    Adjust deps

--- src/main/main.ts
+++ src/main/main.ts
@@ -98,6 +98,7 @@ const createWindow = async () => {
     icon: getAssetPath('icon.png'),
     webPreferences: {
       webSecurity: false,
+      sandbox: false,
       preload: app.isPackaged
         ? path.join(__dirname, 'preload.js')
         : path.join(__dirname, '../../.erb/dll/preload.js'),
